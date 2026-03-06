a2cd5524f4fbbde91b94599b6bb098e9282a286a
ABINASH <ABINASH@LAPTOP-7M3J8POU>
2026-03-06 21:17:23 +0530

    feat: New Logic.

---
+++ src/github_program/GitTest.java
@@ -0,0 +1,28 @@
+package github_program;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+
+        String str = "BIKASH";
+
+        // Time for StringBuilder
+        long startBuilder = System.nanoTime();
+        String revBuilder = new StringBuilder(str).reverse().toString();
+        long endBuilder = System.nanoTime();
+
+        // Time for StringBuffer
+        long startBuffer = System.nanoTime();
+        String revBuffer = new StringBuffer(str).reverse().toString();
+        long endBuffer = System.nanoTime();
+
+        long builderTime = endBuilder - startBuilder;
+        long bufferTime = endBuffer - startBuffer;
+
+        System.out.println("Reverse using StringBuilder: " + revBuilder);
+        System.out.println("Time taken by StringBuilder: " + builderTime + " ns");
+
+        System.out.println("Reverse using StringBuffer: " + revBuffer);
+        System.out.println("Time taken by StringBuffer: " + bufferTime + " ns");
+    }
+}
